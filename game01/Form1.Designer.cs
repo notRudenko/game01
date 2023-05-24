1170ca9a1fb7170b9ca2342763797d80fca0a060
RudenkoDV <rudenkodv@ymail.com>
2023-05-24 18:37:46 +0600

    Add project files.

---
+++ game01/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace game01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bg1 = new System.Windows.Forms.PictureBox();
+            this.banana = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.bg2 = new System.Windows.Forms.PictureBox();
+            this.bg3 = new System.Windows.Forms.PictureBox();
+            this.bg4 = new System.Windows.Forms.PictureBox();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.labelLoose = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.coin = new System.Windows.Forms.PictureBox();
+            this.labelCoins = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bg1
+            // 
+            this.bg1.BackColor = System.Drawing.Color.Transparent;
+            this.bg1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bg1.BackgroundImage")));
+            this.bg1.Image = ((System.Drawing.Image)(resources.GetObject("bg1.Image")));
+            this.bg1.Location = new System.Drawing.Point(49, 36);
+            this.bg1.Name = "bg1";
+            this.bg1.Size = new System.Drawing.Size(800, 600);
+            this.bg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bg1.TabIndex = 0;
+            this.bg1.TabStop = false;
+            // 
+            // banana
+            // 
+            this.banana.BackColor = System.Drawing.Color.Transparent;
+            this.banana.Image = ((System.Drawing.Image)(resources.GetObject("banana.Image")));
+            this.banana.Location = new System.Drawing.Point(400, 500);
+            this.banana.Name = "banana";
+            this.banana.Size = new System.Drawing.Size(50, 30);
+            this.banana.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banana.TabIndex = 1;
+            this.banana.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // bg2
+            // 
+            this.bg2.Image = ((System.Drawing.Image)(resources.GetObject("bg2.Image")));
+            this.bg2.Location = new System.Drawing.Point(0, 800);
+            this.bg2.Name = "bg2";
+            this.bg2.Size = new System.Drawing.Size(800, 600);
+            this.bg2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bg2.TabIndex = 2;
+            this.bg2.TabStop = false;
+            // 
+            // bg3
+            // 
+            this.bg3.Image = ((System.Drawing.Image)(resources.GetObject("bg3.Image")));
+            this.bg3.Location = new System.Drawing.Point(0, 550);
+            this.bg3.Name = "bg3";
+            this.bg3.Size = new System.Drawing.Size(800, 600);
+            this.bg3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bg3.TabIndex = 3;
+            this.bg3.TabStop = false;
+            // 
+            // bg4
+            // 
+            this.bg4.BackColor = System.Drawing.Color.White;
+            this.bg4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bg4.BackgroundImage")));
+            this.bg4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bg4.Location = new System.Drawing.Point(0, -550);
+            this.bg4.Name = "bg4";
+            this.bg4.Size = new System.Drawing.Size(800, 600);
+            this.bg4.TabIndex = 4;
+            this.bg4.TabStop = false;
+            // 
+            // enemy1
+            // 
+            this.enemy1.BackColor = System.Drawing.Color.Transparent;
+            this.enemy1.Image = ((System.Drawing.Image)(resources.GetObject("enemy1.Image")));
+            this.enemy1.Location = new System.Drawing.Point(204, 300);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(60, 50);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 5;
+            this.enemy1.TabStop = false;
+            // 
+            // enemy2
+            // 
+            this.enemy2.BackColor = System.Drawing.Color.Transparent;
+            this.enemy2.Image = ((System.Drawing.Image)(resources.GetObject("enemy2.Image")));
+            this.enemy2.Location = new System.Drawing.Point(518, 285);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(60, 50);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 6;
+            this.enemy2.TabStop = false;
+            // 
+            // labelLoose
+            // 
+            this.labelLoose.AutoSize = true;
+            this.labelLoose.BackColor = System.Drawing.Color.Transparent;
+            this.labelLoose.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLoose.ForeColor = System.Drawing.Color.Red;
+            this.labelLoose.Location = new System.Drawing.Point(340, 376);
+            this.labelLoose.Name = "labelLoose";
+            this.labelLoose.Size = new System.Drawing.Size(118, 42);
+            this.labelLoose.TabIndex = 7;
+            this.labelLoose.Text = "label1";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Silver;
+            this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRestart.Location = new System.Drawing.Point(347, 430);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(103, 35);
+            this.btnRestart.TabIndex = 8;
+            this.btnRestart.Text = "Play Again";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // coin
+            // 
+            this.coin.BackColor = System.Drawing.Color.Gold;
+            this.coin.Location = new System.Drawing.Point(400, -500);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(25, 25);
+            this.coin.TabIndex = 9;
+            this.coin.TabStop = false;
+            // 
+            // labelCoins
+            // 
+            this.labelCoins.AutoSize = true;
+            this.labelCoins.BackColor = System.Drawing.Color.Transparent;
+            this.labelCoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCoins.ForeColor = System.Drawing.Color.Red;
+            this.labelCoins.Location = new System.Drawing.Point(5, 5);
+            this.labelCoins.Name = "labelCoins";
+            this.labelCoins.Size = new System.Drawing.Size(91, 25);
+            this.labelCoins.TabIndex = 10;
+            this.labelCoins.Text = "Coins: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.labelCoins);
+            this.Controls.Add(this.coin);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.labelLoose);
+            this.Controls.Add(this.enemy2);
+            this.Controls.Add(this.enemy1);
+            this.Controls.Add(this.bg3);
+            this.Controls.Add(this.bg2);
+            this.Controls.Add(this.banana);
+            this.Controls.Add(this.bg1);
+            this.Controls.Add(this.bg4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bg1;
+        private System.Windows.Forms.PictureBox banana;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox bg2;
+        private System.Windows.Forms.PictureBox bg3;
+        private System.Windows.Forms.PictureBox bg4;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy2;
+        private System.Windows.Forms.Label labelLoose;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.PictureBox coin;
+        private System.Windows.Forms.Label labelCoins;
+    }
+}
+
---
+++ game01/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game01
+{
+    public partial class Form1 : Form
+    {
+        private Point pos;
+        private bool dragging;
+        private bool lose = false;
+        private int coinsNumber = 0;
+        public int rrr = 45;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bg1.MouseDown += MouseClickDown;
+            bg1.MouseUp += MouseClickUp;
+            bg1.MouseMove += MouseClickMove;
+
+            bg4.MouseDown += MouseClickDown;
+            bg4.MouseUp += MouseClickUp;
+            bg4.MouseMove += MouseClickMove;
+
+            labelLoose.Visible = false;
+            btnRestart.Visible = false;
+
+            KeyPreview = true;
+        }
+
+        private void MouseClickDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            pos.X = e.X;
+            pos.Y = e.Y;
+        }
+        private void MouseClickUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+        private void MouseClickMove(object sender, MouseEventArgs e)
+        {
+            if(dragging == true)
+            {
+                Point currentPoint = PointToScreen(new Point(e.X, e.Y));
+                this.Location = new Point(currentPoint.X - pos.X, currentPoint.Y - pos.Y + bg1.Top);
+            }
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int speed = 5;
+            bg1.Top += speed;
+            bg4.Top += speed;
+
+            int enemySpeed = 7;
+            enemy1.Top += enemySpeed;
+            enemy2.Top += enemySpeed;
+
+            coin.Top += speed;
+
+            if (bg1.Top >= 600)
+            {
+                bg1.Top = 0;
+                bg4.Top = -550;
+            }
+
+            if (enemy1.Top >= 650)
+            {
+                enemy1.Top = -100;
+                Random rand = new Random();
+                enemy1.Left = rand.Next(100, 300);
+            }
+
+            if (enemy2.Top >= 650)
+            {
+                enemy2.Top = -200;
+                Random rand = new Random();
+                enemy2.Left = rand.Next(300, 600);
+            }
+
+            if (coin.Top >= 600)
+            {
+                coin.Top = -50;
+                Random rand = new Random();
+                coin.Left = rand.Next(100, 700);
+            }
+
+            if(banana.Bounds.IntersectsWith(enemy1.Bounds) || banana.Bounds.IntersectsWith(enemy2.Bounds))
+            {
+                timer.Enabled = false;
+                labelLoose.Visible = true;
+                btnRestart.Visible = true;
+                lose = true;
+            }
+
+            if (banana.Bounds.IntersectsWith(coin.Bounds))
+            {
+                coinsNumber++;
+                labelCoins.Text = "Coins: " + coinsNumber.ToString();
+                coin.Top = -50;
+                Random rand = new Random();
+                coin.Left = rand.Next(100, 600);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lose) return;
+            int speed = 10;
+            if(banana.Left>100 && banana.Left < 700)
+            {
+                if (e.KeyCode == Keys.Left)
+                    banana.Left -= 10;
+                else if (e.KeyCode == Keys.Right)
+                    banana.Left += 10;
+            }
+
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            enemy1.Top = -130;
+            enemy2.Top = -300;
+            labelLoose.Visible = false;
+            btnRestart.Visible = false;
+            timer.Enabled = true;
+            lose = false;
+            coinsNumber = 0;
+            labelCoins.Text = "Coins: 0";
+            coin.Top = -500;
+        }
+    }
+}
